1c57cbe5ef7103d6a2b4f393e38bb20374dc4603
Shane <48398988+ShaneDewar@users.noreply.github.com>
2020-05-25 12:04:36 -0400

    Update contentPageJS.js

--- contentPage/contentPageJS.js
+++ contentPage/contentPageJS.js
@@ -1,15 +1,9 @@
 // Attach the button in the HTML to the JS
 var button = document.getElementById("downloadButton");
-//var file = "../92crisscross_smaller.jpg";
 
 // Listen for a click event on the button
 button.addEventListener("click", buttonClicked);
 
 function buttonClicked() {
-  //var toGet = document.createElement('pic');
-  
- // toGet.href = file;
- // toGet.download = file.substr(file.lastIndexOf("/") + 1);
- // toGet.click();
-  window.open("92crisscross_smaller.jpg");  // Download the picture in the window
+  window.open("92crisscross_smaller.jpg");  // Download the picture in new tab
   }
