dd362365ab9882aec119edd4135cdcf7d7128fd5
Shane <48398988+ShaneDewar@users.noreply.github.com>
2020-05-25 11:32:15 -0400

    JS for contentPage

---
+++ contentPage/contentPageJS.js
@@ -0,0 +1,9 @@
+// Attach the button in the HTML to the JS
+var button = document.getElementById("downloadButton");
+
+// Listen for a click event on the button
+button.addEventListener("click", buttonClicked);
+
+function buttonClicked() {
+  window.location = "92crisscross_smaller.jpg";  // Download the picture in the window
+  }
